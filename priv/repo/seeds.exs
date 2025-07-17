1cb52db64935b2f6883a80afcfce7bcbad1171c8
Fly Dev <dev@fly.io>
2025-07-17 08:38:13 +0000

    seed admin user for testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ThunesService.Accounts
+
+# Create admin user for testing
+{:ok, user} = Accounts.register_user(%{email: "admin@example.com"})
+
+{:ok, _user, _expired_tokens} =
+  Accounts.update_user_password(user, %{password: "adminpassword123"})
+
+IO.puts("Seeded admin user: admin@example.com with password: adminpassword123")
