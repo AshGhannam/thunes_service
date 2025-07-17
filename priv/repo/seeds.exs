d23ea25474be487e8481c822951b9b85731db988
Fly Dev <dev@fly.io>
2025-07-17 08:38:32 +0000

    make seed script idempotent

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,7 +13,15 @@
 alias ThunesService.Accounts
 
 # Create admin user for testing
-{:ok, user} = Accounts.register_user(%{email: "admin@example.com"})
+user =
+  case Accounts.get_user_by_email("admin@example.com") do
+    nil ->
+      {:ok, user} = Accounts.register_user(%{email: "admin@example.com"})
+      user
+
+    existing_user ->
+      existing_user
+  end
 
 {:ok, {_user, _expired_tokens}} =
   Accounts.update_user_password(user, %{password: "adminpassword123"})
